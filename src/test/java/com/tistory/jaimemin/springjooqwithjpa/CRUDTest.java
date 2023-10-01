e43c538902494a212077c11fa14c9fcb32b9f76b
jaimemin <jaimemin@gmail.com>
2023-10-02 03:40:07 +0900

    applied jooq to jpa project

--- src/test/java/com/tistory/jaimemin/springjooqwithjpa/CRUDTest.java
+++ src/test/java/com/tistory/jaimemin/springjooqwithjpa/CRUDTest.java
@@ -5,15 +5,17 @@ import com.tistory.jaimemin.jooq.code.jooq.tables.records.TeamRecord;
 import com.tistory.jaimemin.springjooqwithjpa.entity.Member;
 import com.tistory.jaimemin.springjooqwithjpa.entity.Team;
 import com.tistory.jaimemin.springjooqwithjpa.repository.TeamJPARepository;
-import org.jooq.DSLContext;
-import org.jooq.InsertSetMoreStep;
+import org.jooq.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 import static com.tistory.jaimemin.jooq.code.jooq.tables.Member.MEMBER;
 import static com.tistory.jaimemin.jooq.code.jooq.tables.Team.TEAM;
@@ -22,7 +24,7 @@ import static com.tistory.jaimemin.jooq.code.jooq.tables.Team.TEAM;
 @SpringBootTest
 public class CRUDTest {
 
-    private static int TEAM_CNT = 1000;
+    private static int TEAM_CNT = 10000;
 
     private static int PAGE_SIZE = 1000;
 
@@ -58,25 +60,75 @@ public class CRUDTest {
 
     @Test
     void insertTeams_with_jooq() {
-        for (int page = 0; page < (int) Math.ceil((double) TEAM_CNT / PAGE_SIZE); page++) {
-            List<InsertSetMoreStep<TeamRecord>> teamInserts = new ArrayList<>();
-            List<InsertSetMoreStep<MemberRecord>> memberInserts = new ArrayList<>();
+        List<TeamRecord> teamRecords = new ArrayList<>();
+        List<MemberRecord> memberRecords = new ArrayList<>();
+        Map<String, String> memberName2teamName = new HashMap<>();
+        List<String> teamNames = new ArrayList<>();
 
+        for (int page = 0; page < (int) Math.ceil((double) TEAM_CNT / PAGE_SIZE); page++) {
             for (int i = 0; i < PAGE_SIZE; i++) {
-                TeamRecord teamRecord = dslContext.newRecord(TEAM);
+                TeamRecord teamRecord = new TeamRecord();
                 teamRecord.setName("Team" + (i * (page + 1) + 1));
-                teamInserts.add(dslContext.insertInto(TEAM).set(teamRecord));
+                teamRecords.add(teamRecord);
+                teamNames.add(teamRecord.getName());
 
                 for (int j = 0; j < MEMBER_CNT_PER_TEAM; j++) {
-                    MemberRecord memberRecord = dslContext.newRecord(MEMBER);
+                    MemberRecord memberRecord = new MemberRecord();
                     memberRecord.setName("Member " + (i * (page + 1) * 10 + (j + 1)));
-                    memberRecord.setTeamId(teamRecord.getId()); // Assuming Team has an ID field
-                    memberInserts.add(dslContext.insertInto(MEMBER).set(memberRecord));
+                    memberRecords.add(memberRecord);
+                    memberName2teamName.put(memberRecord.getName(), teamRecord.getName());
                 }
             }
-
-            dslContext.batch(teamInserts).execute();
-            dslContext.batch(memberInserts).execute();
         }
+
+        // Batch insert teams and get generated team IDs
+        dslContext.batchInsert(teamRecords).execute();
+        // Fetch generated team IDs
+        Result<TeamRecord> generatedTeams = dslContext
+                .selectFrom(TEAM)
+                .where(TEAM.NAME.in(teamNames))
+                .fetch();
+        Map<String, Long> teamName2id = new HashMap<>();
+
+        for (TeamRecord teamRecord : generatedTeams) {
+            teamName2id.put(teamRecord.getName(), teamRecord.getId());
+        }
+
+        for (MemberRecord memberRecord : memberRecords) {
+            memberRecord.setTeamId(teamName2id.get(memberName2teamName.get(memberRecord.getName())));
+        }
+
+        // Batch insert members
+        dslContext.batchInsert(memberRecords).execute();
     }
+
+
+
+    @Test
+    void insertTeams_with_jooq_single_insert() {
+        List<Long> generatedTeamIds = new ArrayList<>();
+
+        for (int page = 0; page < (int) Math.ceil((double) TEAM_CNT / PAGE_SIZE); page++) {
+            for (int i = 0; i < PAGE_SIZE; i++) {
+                // Insert Team data and get generated ID
+                Long teamId = dslContext.insertInto(TEAM, TEAM.NAME)
+                        .values("Team" + (i * (page + 1) + 1))
+                        .returning(TEAM.ID)
+                        .fetchOne()
+                        .getId();
+                generatedTeamIds.add(teamId);
+
+                // Insert Member data with the obtained Team ID
+                for (int j = 0; j < MEMBER_CNT_PER_TEAM; j++) {
+                    dslContext.insertInto(MEMBER, MEMBER.NAME, MEMBER.TEAM_ID)
+                            .values("Member " + (i * (page + 1) * 10 + (j + 1)), teamId)
+                            .execute();
+                }
+            }
+        }
+
+        // Now, generatedTeamIds contains the generated team IDs
+    }
+
+
 }
